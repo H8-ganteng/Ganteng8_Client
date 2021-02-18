7b156e83f0ccd0da339af84f4d28beb6bfec0119
Zalfa <zalfa.muhammad@gmail.com>
2021-02-18 23:50:28 +0700

    nambah page error

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'https://ganteng8server.herokuapp.com/',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
 
 new Vue({
   router,
